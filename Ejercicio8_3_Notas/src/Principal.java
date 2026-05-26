3795584b912c88482a53465bed41e015cdaccb73
anghelosantiago <asantiagod@unal.edu.co>
2026-05-25 23:03:17 -0500

    Delete Ejercicio8_2_Notas/src directory

--- Ejercicio8_3_Notas/src/Principal.java
+++
@@ -1,9 +0,0 @@
-package Notas;
-
-public class Principal {
-    public static void main(String[] args) {
-        VentanaPrincipal miVentanaPrincipal;
-        miVentanaPrincipal = new VentanaPrincipal();
-        miVentanaPrincipal.setVisible(true);
-    }
-}
